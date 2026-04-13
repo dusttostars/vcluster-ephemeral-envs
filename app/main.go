4b9ebeff287955a0d90f17d891a6e65b7975fd10
Waka <doblealberto@outloook.com>
2026-04-13 14:50:47 -0500

    feat: toy app v1

--- app/main.go
+++ app/main.go
@@ -41,7 +41,7 @@ func main() {
 </head>
 <body>
   <div class="card">
-    <h1><span class="pulse"></span>Ephemeral App</h1>
+    <h1><span class="pulse"></span>Toy App v1</h1>
     <p class="subtitle">Running inside a vCluster</p>
     <div class="row"><span class="label">Hostname</span><span class="value">%s</span></div>
     <div class="row"><span class="label">Branch</span><span class="value">%s</span></div>
